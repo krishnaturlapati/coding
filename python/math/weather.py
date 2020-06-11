435ece9d07123fef99a7a0ac36a298643a825220
Venkat Krishna Turlapati <turlapati.venkat.krishna@outlook.com>
2020-06-10 23:35:17 -0400

    Weather conversion TDD problem

---
+++ python/math/weather.py
@@ -0,0 +1,41 @@
+"""
+Develop a Python module called weather.py that contains a temperature class and associated
+methods.
+
+Your module should contain the necessary Exceptions
+required to signal when the user requests a temperature
+that is below absolute zero.
+
+Furthermore, your temperature class should have methods
+that convert the class’s temperature value (attribute) to Celsius
+and to Fahrenheit.
+
+Reference https://courses.ece.msstate.edu/ece4723/fall17/lab/t1/t1.pdf
+
+"""
+
+
+
+
+class Temperature:
+    class OutOfRangeError(ValueError):
+        pass
+
+    def __init__(self, celsius=0, fahrenheit=0):
+        self._celsius = celsius
+        self._fahrenheit = fahrenheit
+        if self._celsius < -273.15:
+            raise self.OutOfRangeError('celsius cannot got below absolute zero ')
+
+        if self._fahrenheit < -459.67:
+            raise self.OutOfRangeError('fahrenheit cannot got below absolute zero ')
+
+
+    def fahrenheit(self):
+        return 9.0/5.0 * self._celsius + 32
+
+    def celsius(self):
+        return (self._fahrenheit - 32) * 5.0/9.0
+
+
+
---
+++ tests/python/math/test_temperature.py
@@ -0,0 +1,58 @@
+from python.math.weather import Temperature
+import pytest
+import unittest
+assertions = unittest.TestCase('__init__')
+
+SIGNIFICANT_PLACES = 4
+
+knownValues = ( (-273.15, -459.67),	# absolute zero
+                (-89, -128.2),	# coldest recorded WX temp on earth's surface
+                (-17.777777777, 0),	# temp of Daniel Gabriel Fahrenheit's ice/salt mixture
+                (0, 32),		# H20 freezes
+                (15, 59),		# average earth WX temp
+                (36.8, 98.24),	# average body temperature
+                (58, 136.4),	# warmest recorded WX temp on earth's surface
+                (100, 212),		# H20 boils
+                (5535, 9995) )	# average WX temp on the sun's surface
+
+
+def test_to_fahrenheit_known_values():
+    """converting to Fahrenheit should give known result with known input"""
+    for tempC, tempF in knownValues:
+        result = Temperature(celsius=tempC).fahrenheit()
+        assertions.assertAlmostEquals(tempF, result, SIGNIFICANT_PLACES)
+
+
+def test_to_celsius_known_values():
+    """converting from Fahrenheit should give known back our known inputs"""
+    for tempC, tempF in knownValues:
+        result = Temperature(fahrenheit=tempF).celsius()
+        assertions.assertAlmostEqual(tempC, result, SIGNIFICANT_PLACES)
+
+
+def test_sanity_one():
+    """Celsius(Fahrenheit(n))==n for all n"""
+    for tempC in range(-273, 6000):
+        tempF = Temperature(celsius=tempC).fahrenheit()
+        result = Temperature(fahrenheit=tempF).celsius()
+        assertions.assertAlmostEqual(tempC, result, SIGNIFICANT_PLACES)
+
+
+def test_sanity_two():
+        """Fahrenheit(Celsius(n))==n for all n"""
+        for tempF in range(-459, 10000):
+            tempC = Temperature(fahrenheit=tempF).celsius()
+            result = Temperature(celsius=tempC).fahrenheit()
+            assertions.assertAlmostEqual(tempF, result, SIGNIFICANT_PLACES)
+
+
+def test_too_small_cel():
+    """to Fahrenheit should fail with excessively negative Celsius input
+    (below absolute zero)"""
+    assertions.assertRaises(Temperature.OutOfRangeError, Temperature, celsius=-273.151)
+
+
+def test_too_small_fah():
+    """to Celsius should fail with excessively negative Fahrenheit input
+    (below absolute zero)"""
+    assertions.assertRaises(Temperature.OutOfRangeError, Temperature, fahrenheit=-459.671)
